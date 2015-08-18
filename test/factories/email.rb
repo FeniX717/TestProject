f5835339018b88fd08391f19802610533a62081e
Kostya <konstantin.mikhaskov@gmail.com>
2015-08-18 22:26:14 +0300

    Remove duplicated directory

--- test/factories/email.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :email do
-    subject "My TestProject"
-    text_part "Tests for project"
-    from { Faker::Internet.email }
-    to { Faker::Internet.email }
-    date
-  end
-end
--- test/models/create_mail_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CreateMailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
